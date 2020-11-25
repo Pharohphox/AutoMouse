bab442e044896cbf0807aa4b0bdaa22bb1ce2b67
Pharohphox <pharohphox@hotmail.com>
2020-11-25 17:07:06 -0500

    Initial commit and push

---
+++ AutoMouse/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AutoMouse
+{
+    partial class Clicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TimesClicked = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblEndTime = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStartTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(13, 24);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(21, 13);
+            this.StatusLabel.TabIndex = 2;
+            this.StatusLabel.Text = "Off";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // TimesClicked
+            // 
+            this.TimesClicked.AutoSize = true;
+            this.TimesClicked.Location = new System.Drawing.Point(74, 87);
+            this.TimesClicked.Name = "TimesClicked";
+            this.TimesClicked.Size = new System.Drawing.Size(13, 13);
+            this.TimesClicked.TabIndex = 5;
+            this.TimesClicked.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "# Clicked:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "End Time:";
+            // 
+            // lblEndTime
+            // 
+            this.lblEndTime.AutoSize = true;
+            this.lblEndTime.Location = new System.Drawing.Point(173, 112);
+            this.lblEndTime.Name = "lblEndTime";
+            this.lblEndTime.Size = new System.Drawing.Size(0, 13);
+            this.lblEndTime.TabIndex = 8;
+            this.lblEndTime.UseMnemonic = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Start Time:";
+            // 
+            // lblStartTime
+            // 
+            this.lblStartTime.AutoSize = true;
+            this.lblStartTime.Location = new System.Drawing.Point(173, 86);
+            this.lblStartTime.Name = "lblStartTime";
+            this.lblStartTime.Size = new System.Drawing.Size(0, 13);
+            this.lblStartTime.TabIndex = 10;
+            // 
+            // Clicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 134);
+            this.Controls.Add(this.lblStartTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblEndTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TimesClicked);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(282, 173);
+            this.MinimumSize = new System.Drawing.Size(282, 173);
+            this.Name = "Clicker";
+            this.ShowIcon = false;
+            this.Text = "Clicker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Clicker_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TimesClicked;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblEndTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStartTime;
+    }
+}
+
---
+++ AutoMouse/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+
+namespace AutoMouse
+{
+    public partial class Clicker : Form
+    {
+        InterruptibleLoop il = new InterruptibleLoop();
+        private Thread checkForUpdate;
+        private volatile bool loopControl;
+        private delegate void delUpdateTimesClicked(string updateString, string myEndTime);
+
+        public Clicker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StatusLabel.Text = "ON";
+            StatusLabel.BackColor = Color.Green;
+            ClickAway();
+            StartCheck();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            StatusLabel.Text = "OFF";
+            StatusLabel.BackColor = Color.Red;
+            TimesClicked.Text = il.count.ToString();
+            il.Stop();
+            EndCheck();
+            lblStartTime.Text = string.Empty;
+            lblEndTime.Text = string.Empty;
+        }
+
+        private void ClickAway()
+        {
+            int wait;
+
+            int.TryParse(textBox1.Text, out wait);
+            lblStartTime.Text = DateTime.Now.ToShortTimeString();
+            il.Start(wait);
+        }
+
+        private void CheckForUpdate()
+        {
+            delUpdateTimesClicked DelUpdateTimesClicked = new delUpdateTimesClicked(UpdateTimesClicked);
+            
+            while(!loopControl)
+            {
+                this.TimesClicked.BeginInvoke(DelUpdateTimesClicked, il.count.ToString(), il.endTime.ToShortTimeString());
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void StartCheck()
+        {
+            if (checkForUpdate != null)
+            {
+                return;
+            }
+
+            loopControl = false;
+
+            checkForUpdate = new Thread(CheckForUpdate);
+            checkForUpdate.Start();
+        }
+
+        private void EndCheck()
+        {
+            if(checkForUpdate == null)
+            {
+                return;
+            }
+
+            loopControl = true;
+            checkForUpdate.Join();
+
+            checkForUpdate = null;
+        }
+
+        public void UpdateTimesClicked(string updateString, string myEndTime)
+        {
+            TimesClicked.Text = updateString;
+            lblEndTime.Text = myEndTime;
+        }
+
+        private void Clicker_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            EndCheck();
+        }
+    }
+
+    public class InterruptibleLoop
+    {
+        private volatile bool stopLoop;
+        private Thread loopThread;
+        private int _wait;
+        public int count;
+        public DateTime endTime;
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void Start(int waiter)
+        {
+            _wait = waiter;
+
+            // If the thread is already running, do nothing.
+            if (loopThread != null)
+            {
+                return;
+            }
+
+            // Reset the "stop loop" signal.
+            stopLoop = false;
+
+            // Create and start the new thread.
+            loopThread = new Thread(LoopBody);
+            loopThread.Start();
+        }
+
+        public void Stop()
+        {
+            // If the thread is not running, do nothing.
+            if (loopThread == null)
+            {
+                return;
+            }
+
+            // Signal to the thread that it should stop looping.
+            stopLoop = true;
+
+            // Wait for the thread to terminate.
+            loopThread.Join();
+
+            loopThread = null;
+        }
+
+        private void LoopBody()
+        {
+            count = 0;
+            DateTime myTime = DateTime.Now.AddMinutes((double)_wait);
+            endTime = myTime;
+
+            Thread.Sleep(5000);
+
+            while (!stopLoop && DateTime.Now <= myTime )
+            {
+                // Do your work here
+                DoMouseClick();
+                count++;
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+    }
+}
